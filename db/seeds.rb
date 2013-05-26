cbfacd61d76f8e23108ce15708be984407a72dfd
Ruoran Liu <rrliu@thoughtworks.com>
2013-05-26 16:02:57 +0800

    Construct tech radar database example

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,7 +37,24 @@ trial_techniques = Node.create({node_descriptor: assessment_descriptor})
 adopt_techniques_title_field = Field.create({field_descriptor: title_field_descriptor, node: trial_techniques, data: "Techniques - Trial"})
 adopt_techniques_content_field = Field.create({field_descriptor: content_field_descriptor, node: trial_techniques, data: "Trial techniques."})
 
+aggregates_as_documents = Node.create({node_descriptor: technology_descriptor})
+aggregates_as_documents_title_field = Field.create({field_descriptor: title_field_descriptor, node: aggregates_as_documents, data: "Aggregates as documents"})
+aggregates_as_documents_content_field = Field.create({field_descriptor: content_field_descriptor, node: aggregates_as_documents, data: "No explanation."})
+
+automated_deployment_pipeline = Node.create({node_descriptor: technology_descriptor})
+automated_deployment_pipeline_title_field = Field.create({field_descriptor: title_field_descriptor, node: automated_deployment_pipeline, data: "Automated deployment pipeline"})
+automated_deployment_pipeline_content_field = Field.create({field_descriptor: content_field_descriptor, node: automated_deployment_pipeline, data: "No explanation."})
+
+analyzing_test_runs = Node.create({node_descriptor: technology_descriptor})
+analyzing_test_runs_title_field = Field.create({field_descriptor: title_field_descriptor, node: analyzing_test_runs, data: "Analyzing test runs"})
+analyzing_test_runs_content_field = Field.create({field_descriptor: content_field_descriptor, node: analyzing_test_runs, data: "Failing tests reveal bugs in production..."})
+
 techniques.child_nodes.push adopt_techniques
 techniques.child_nodes.push trial_techniques
+
+adopt_techniques.push aggregates_as_documents
+adopt_techniques.push automated_deployment_pipeline
+
+trial_techniques.push analyzing_test_runs
 
 
