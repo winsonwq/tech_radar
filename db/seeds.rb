40bc23df64f7257f5a4a0e7e2d6fb4446e18fc4b
Wang Qiu <winsonwq@gmail.com>
2013-05-28 17:52:23 +0800

    techradar data draft. Qiu

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require "yaml"
+
 category_descriptor = NodeDescriptor.create({name: 'Category'})
 assessment_descriptor = NodeDescriptor.create({name: 'Assessment'})
 technology_descriptor = NodeDescriptor.create({name: 'Technology'})
@@ -17,8 +19,8 @@ category_descriptor.child_node_descriptors.push assessment_descriptor
 assessment_descriptor.child_node_descriptors.push technology_descriptor
 
 # TODO: support creating relation like this
-RelationDescriptor.create({name: 'Contains multiple assessments', parent_node_descriptor: category_descriptor, child_node_descriptor: assessment_descriptor})
-RelationDescriptor.create({name: 'Contains multiple technologies', parent_node_descriptor: assessment_descriptor, child_node_descriptor: technology_descriptor})
+# RelationDescriptor.create({name: 'Contains multiple assessments', parent_node_descriptor: category_descriptor, child_node_descriptor: assessment_descriptor})
+# RelationDescriptor.create({name: 'Contains multiple technologies', parent_node_descriptor: assessment_descriptor, child_node_descriptor: technology_descriptor})
 
 category_descriptor.field_descriptors.push title_field_descriptor
 category_descriptor.field_descriptors.push content_field_descriptor
@@ -29,49 +31,26 @@ assessment_descriptor.field_descriptors.push content_field_descriptor
 technology_descriptor.field_descriptors.push title_field_descriptor
 technology_descriptor.field_descriptors.push content_field_descriptor
 
-category_clazz = Global::Category
-assessment_clazz = Global::Assessment
-technology_clazz = Global::Technology
+dirname = File.dirname(File.expand_path(__FILE__))
 
-techniques = category_clazz.new
-techniques.title = "Techniques"
-techniques.content = "Techniques trends."
+%w{ techniques.yml languages.yml tools.yml platforms.yml }.each do |file|
+  file_path = File.join(dirname, file)
+  source = YAML::load_file(file_path)
 
-adopt_techniques = assessment_clazz.new
-adopt_techniques.title = "Techniques - Adopt"
-adopt_techniques.content = "Adopt techniques"
+  category = Global::Category.new
 
-trial_techniques = assessment_clazz.new
-trial_techniques.title = "Techniques - Trial"
-trial_techniques.content = "Trial techniques"
+  source[source.keys.first].each do |key, arr|
+    assess = Global::Assessment.new
+    category.add assess
 
-aggregates_as_documents = technology_clazz.new
-aggregates_as_documents.title = "Aggregates as documents"
-aggregates_as_documents.content = "No explanation"
+    arr.each do |item|
+      tech = Global::Technology.new
+      tech.title = item["title"]
+      tech.content = item["content"]
+      assess.add tech
+    end
+  end
+end
 
-analyzing_test_runs = technology_clazz.new
-analyzing_test_runs.title = "Analyzing test runs"
-analyzing_test_runs.content = "Failing tests reveal bugs in production..."
-
-techniques.add adopt_techniques
-techniques.add trial_techniques
-
-adopt_techniques.add aggregates_as_documents
-trial_techniques.add analyzing_test_runs
-
-tools = category_clazz.new
-tools.title = "Tools"
-tools.content = "Tools Trends"
-
-adopt_tools = assessment_clazz.new
-adopt_tools.title = "Tools - Adopt"
-adopt_tools.content = "Adopt tools"
-
-d3 = technology_clazz.new
-d3.title = "D3"
-d3.content = "D3 continues to gain traction as..."
-
-tools.add adopt_tools
-adopt_tools.add d3
-
-
+# TODO: Import latest tech radar info
+# TODO: Navigation Logic
