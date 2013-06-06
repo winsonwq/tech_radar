5bb87394502acbc12fd4b4bc6aec9e650dd2a4dd
Ruoran Liu <rrliu@thoughtworks.com>
2013-06-06 13:41:12 +0800

    Fix build as add validation params in test

--- spec/controllers/weixin_controller_spec.rb
+++ spec/controllers/weixin_controller_spec.rb
@@ -3,12 +3,18 @@ require 'xmlsimple'
 
 include TechRadar
 
-describe WeixinController do
 
+
+describe WeixinController do
+  let!(:validation) do
+    {:signature => "1dd201087b718bc77eb5665ebe0491050934eea5",
+     :timestamp => "1370490314",
+     :nonce => "1371030824"}
+  end
   describe :index do
 
     it "should return message in xml format" do
-      raw_post :index, {}, Weixin.xml_gen("hello", true)
+      raw_post :index, validation, Weixin.xml_gen("hello", true)
       response.headers['Content-Type'].should include "application/xml"
     end
   end
