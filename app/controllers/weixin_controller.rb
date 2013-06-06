8fb3b12a597ffa8c5bcaeb73fae6caa0a1311a4c
Ruoran Liu <rrliu@thoughtworks.com>
2013-06-06 10:51:15 +0800

    Set token validation. Ruoran

--- app/controllers/weixin_controller.rb
+++ app/controllers/weixin_controller.rb
@@ -2,8 +2,16 @@ class WeixinController < ApplicationController
 
   layout false
 
+  before_filter :validate_weixin_token
+
   def index
     render xml: Weixin.gen_response_body(request.body)
+  end
+
+  private
+  def validate_weixin_token
+    array = [Rails.configuration.token, params[:timestamp], params[:nonce]].sort
+    render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module TechRadar
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.token = 'areyougoingtoscarboroughfair'
   end
 end
