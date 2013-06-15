6a023eea4cc166ee8223971ff1507b6f23901c3b
Wang Qiu <winsonwq@gmail.com>
2013-06-15 16:05:21 +0800

    record latest message of user in session. Qiu

--- app/controllers/weixin_controller.rb
+++ app/controllers/weixin_controller.rb
@@ -1,11 +1,14 @@
+require 'xmlsimple'
+
 class WeixinController < ApplicationController
 
   layout false
 
-  before_filter :validate_weixin_token
+  before_filter :validate_weixin_token, :parse_message
+  after_filter :update_session
 
   def index
-    render xml: Weixin.gen_response_body(request.body)
+    render xml: Weixin.gen_response_body(@message)
   end
 
   def validate_echostr
@@ -13,6 +16,15 @@ class WeixinController < ApplicationController
   end
 
   private
+
+  def parse_message
+    @message = Weixin.parse request.body
+  end
+
+  def update_session
+    Session.safe_get(@message[:from])[:latest_message] = @message[:content]
+  end
+
   def validate_weixin_token
     array = [Rails.configuration.token, params[:timestamp], params[:nonce]].sort
     render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
--- spec/controllers/weixin_controller_spec.rb
+++ spec/controllers/weixin_controller_spec.rb
@@ -9,11 +9,23 @@ describe WeixinController do
      :timestamp => "1370490314",
      :nonce => "1371030824"}
   end
-  describe :index do
 
+  describe :index do
     it "should return message in xml format" do
       raw_post :index, validation, Weixin.xml_gen("hello")
       response.headers['Content-Type'].should include "application/xml"
+    end
+  end
+
+  describe :session do
+
+    before :all do
+      Session.clear
+    end
+
+    it "should create session record" do
+      raw_post :index, validation, Weixin.xml_gen("hello", "from", "to")
+      Session.get("from")[:latest_message].should == "hello"
     end
   end
 
