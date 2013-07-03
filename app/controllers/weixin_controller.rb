094fc5883f26c94441aa078f64240b40b1d2220c
Ruoran Liu <rrliu@thoughtworks.com>
2013-07-03 15:17:08 +0800

    Allow CORS. Ruoran

--- app/controllers/weixin_controller.rb
+++ app/controllers/weixin_controller.rb
@@ -7,6 +7,9 @@ class WeixinController < ApplicationController
   before_filter :validate_weixin_token, :parse_message, :except => :technology
 
   def index
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST'
+    response.headers['Access-Control-Max-Age'] = '60'
     render xml: Weixin.gen_response_body(@message)
   end
 
